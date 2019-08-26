05b6c8e28e5f0a0a420d90f140257576d784af3c
Arnold Kibira <arnold.ril3y@gmail.com>
2019-08-26 09:42:07 +0300

    export user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,7 +2,7 @@ export class User {
     static result: any;
     // tslint:disable-next-line: variable-name
     constructor(public url: string, public login: string, public html_url: string, public location: string,
-                // tslint:disable-next-line: variable-name
+                // tslint:disable-next-line: variable-name .
                 public public_repos: number, public followers: number, public following: number, public avatar_url: string) {
 
     }
